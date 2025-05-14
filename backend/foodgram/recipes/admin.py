d3acddf44593efa33b58086214c2e2aa589d709a
Poluianov Valeriy <bestcom5055@gmail.com>
2025-05-14 03:22:16 +0300

    Improve: Admin - more displays and new filters.
    Added RecipeInline in UserAdmin;
    Added new calculated fields in UserAdmin, IngredientAdmin, RecipeAdmin;
    Added new custom filters (`admin_filters.py`) in UserAdmin and RecipeAdmin.

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,8 +1,16 @@
+from django.db.models import Count
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.safestring import mark_safe
 
-from .models import (User, Subscription, Ingredient, Recipe,
-                     RecipeIngredient, Favorite, ShoppingCart)
+from .models import (
+    User, Subscription, Ingredient, Recipe, RecipeIngredient,
+    Favorite, ShoppingCart
+)
+from .admin_filters import (
+    RecipeCountFilter, SubscriberCountFilter, SubscriptionCountFilter,
+    UsedInRecipesCountFilter, CookingTimeFilter
+)
 
 
 # Users.
@@ -36,12 +44,50 @@ class SubscriptionInline(admin.TabularInline):
     raw_id_fields = ('author',)
 
 
+class RecipeInline(admin.TabularInline):
+    model = Recipe
+    extra = 0
+    verbose_name = 'Рецепт'
+    verbose_name_plural = 'Рецепты'
+
+
+class FavoriteInline(admin.TabularInline):
+    model = Favorite
+    extra = 0
+    verbose_name = 'Избранный рецепт'
+    verbose_name_plural = 'Избранное'
+
+
+class ShoppingCartInline(admin.TabularInline):
+    model = ShoppingCart
+    extra = 0
+    verbose_name = 'Рецепт в корзине'
+    verbose_name_plural = 'Корзина покупок'
+
+
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    inlines = [FollowerInline, SubscriptionInline]
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    inlines = [FollowerInline,
+               SubscriptionInline,
+               RecipeInline,
+               FavoriteInline,
+               ShoppingCartInline,]
+    list_display = ('id',
+                    'username',
+                    'full_name',
+                    'email',
+                    'avatar_preview',
+                    'recipe_count',
+                    'subscriptions_count',
+                    'subscribers_count',
+                    'is_staff',)
     list_display_links = ('username', 'email')
-    list_filter = ('is_staff', 'is_active', 'date_joined')
+    list_filter = ('date_joined',
+                   RecipeCountFilter,
+                   SubscriptionCountFilter,
+                   SubscriberCountFilter,
+                   'is_staff',
+                   'is_active',)
     search_fields = ('email', 'username', 'first_name', 'last_name')
     ordering = ('-date_joined',)
     readonly_fields = ('last_login', 'date_joined')
@@ -70,14 +116,55 @@ class UserAdmin(DjangoUserAdmin):
         }),
     )
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            recipe_count=Count('recipes', distinct=True),
+            subscriptions_count=Count('subscriptions', distinct=True),
+            subscribers_count=Count('authors', distinct=True),
+        )
+
+    @admin.display(description='Имя и фамилия')
+    def full_name(self, user):
+        return user.get_full_name()
+
+    @mark_safe
+    @admin.display(description='Аватар')
+    def avatar_preview(self, user):
+        if user.avatar:
+            return (f'<img src="{user.avatar.url}"'
+                    f'style="height: 30px; width: 30px; border-radius: 50%;">')
+        return '-'
+
+    @admin.display(description='Рецептов')
+    def recipe_count(self, user):
+        return user.recipe_count
+
+    @admin.display(description='Подписок')
+    def subscriptions_count(self, user):
+        return user.subscriptions_count
+
+    @admin.display(description='Подписчиков')
+    def subscribers_count(self, user):
+        return user.subscribers_count
+
 
 # Recipes.
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'measurement_unit')
+    list_display = ('name', 'measurement_unit', 'used_in_recipes_count')
     list_display_links = ('name',)
+    list_filter = ('measurement_unit', UsedInRecipesCountFilter)
     search_fields = ('name', 'measurement_unit')
     ordering = ('name',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            used_in_recipes_count=Count('recipes', distinct=True),
+        )
+
+    @admin.display(description='Использован в рецептах')
+    def used_in_recipes_count(self, ingredient):
+        return ingredient.used_in_recipes_count
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -91,12 +178,38 @@ class RecipeIngredientInline(admin.TabularInline):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline,]
-    list_display = ('name', 'author', 'cooking_time', 'created_at')
+    list_display = ('id',
+                    'name',
+                    'cooking_time',
+                    'author',
+                    'favorited_count',
+                    'get_ingredients',
+                    'image_preview')
     list_display_links = ('name',)
-    list_filter = ('name', 'author', 'cooking_time')
+    list_filter = (CookingTimeFilter,)
     search_fields = ('name', 'ingredients__name')
     readonly_fields = ('created_at',)
     ordering = ('-created_at',)
+
+    @admin.display(description='В избранном')
+    def favorited_count(self, recipe):
+        return recipe.favorites.count()
+
+    @mark_safe
+    @admin.display(description='Ингредиенты')
+    def get_ingredients(self, recipe):
+        return '<br>'.join(
+            f'- {item.ingredient.name} {item.amount} '
+            f'{item.ingredient.measurement_unit}'
+            for item in recipe.ingredients_amounts.all()
+        )
+
+    @mark_safe
+    @admin.display(description='Изображение')
+    def image_preview(self, recipe):
+        if recipe.image:
+            return f'<img src="{recipe.image.url}" style="height: 100px">'
+        return '-'
 
 
 @admin.register(Favorite, ShoppingCart)
@@ -105,26 +218,3 @@ class UserRecipeAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe')
     list_display_links = ('user', 'recipe')
     search_fields = ('user__username', 'recipe__name')
-
-
-# Expand the user admin-panel.
-admin.site.unregister(User)
-
-
-class FavoriteInline(admin.TabularInline):
-    model = Favorite
-    extra = 0
-    verbose_name = 'Избранный рецепт'
-    verbose_name_plural = 'Избранное'
-
-
-class ShoppingCartInline(admin.TabularInline):
-    model = ShoppingCart
-    extra = 0
-    verbose_name = 'Рецепт в корзине'
-    verbose_name_plural = 'Корзина покупок'
-
-
-@admin.register(User)
-class UserAdmin(UserAdmin):
-    inlines = UserAdmin.inlines + [FavoriteInline, ShoppingCartInline]
---
+++ backend/foodgram/recipes/admin_filters.py
@@ -0,0 +1,99 @@
+from django.contrib.admin import SimpleListFilter
+from django.db.models import Avg
+
+
+class AbstractNumberFilter(SimpleListFilter):
+    """Base class for count filters.
+
+    Basically, provides filters:
+    - Empty (0)
+    - Not empty (>0)
+    - A little (<N)
+    - Medium (>N && <M)
+    - A lot (>M)
+    where N and M are calculated as:
+        N = average_count * 0.75
+        M = average_count * 1.25
+
+    Also, in the URL you can specify your own value in the format of the type:
+        /?parameter_name=>X
+        /?parameter_name=<X
+        /?parameter_name=X
+    where X is a number.
+    You can also separate several values with a comma, for example:
+        /?parameter_name=>2,<5
+    will return values in range (2,5).
+
+    To make custom filters, override the `_get_lookups` method.
+    """
+
+    title = None
+    parameter_name = None
+
+    @staticmethod
+    def _get_lookups(n, m):
+        return [
+            ('0', 'Нет'),
+            ('>0', 'Есть'),
+            (f'<{n}', f'Мало - меньше {n}'),
+            (f'>{n - 1},<{m + 1}', f'Средне - от {n} до {m}'),
+            (f'>{m}', f'Много - больше {m}'),
+        ]
+
+    def lookups(self, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        avg = queryset.aggregate(avg=Avg(self.parameter_name))['avg'] or 1
+        n = int(avg * 0.75) or 1
+        m = int(avg * 1.25) if n > 1 else 2
+        return self._get_lookups(n, m)
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if not value:
+            return queryset
+
+        filters = {}
+        for part in value.split(','):
+            try:
+                if part.startswith('>'):
+                    filters[f'{self.parameter_name}__gt'] = int(part[1:])
+                elif part.startswith('<'):
+                    filters[f'{self.parameter_name}__lt'] = int(part[1:])
+                elif part.isdigit():
+                    filters[self.parameter_name] = int(part)
+            except ValueError:
+                continue  # Ignore invalid values.
+        return queryset.filter(**filters)
+
+
+class RecipeCountFilter(AbstractNumberFilter):
+    title = 'Рецепты'
+    parameter_name = 'recipe_count'
+
+
+class SubscriptionCountFilter(AbstractNumberFilter):
+    title = 'Подписки'
+    parameter_name = 'subscriptions_count'
+
+
+class SubscriberCountFilter(AbstractNumberFilter):
+    title = 'Подписчики'
+    parameter_name = 'subscribers_count'
+
+
+class UsedInRecipesCountFilter(AbstractNumberFilter):
+    title = 'Использован в рецептах'
+    parameter_name = 'used_in_recipes_count'
+
+
+class CookingTimeFilter(AbstractNumberFilter):
+    title = 'Время приготовления'
+    parameter_name = 'cooking_time'
+
+    @staticmethod
+    def _get_lookups(n, m):
+        return [
+            (f'<{n}', f'Быстро - быстрее {n} минут'),
+            (f'>{n - 1},<{m + 1}', f'Средне - от {n} до {m} минут'),
+            (f'>{m}', f'Долго - дольше {m} минут'),
+        ]
